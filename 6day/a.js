e70cf3143a07ed68e2ebb75a1afcddfe07e686b2
wangxiaojin <wangxj@xcbobo.com>
2018-05-31 10:59:49 +0800

    add

---
+++ 6day/a.js
@@ -0,0 +1,3 @@
+export let str1="zf";
+export let str2="zf2";
+//导出的结果{str1:"zf",str2:"zf2"}
--- 6day/main.js
+++ 6day/main.js
@@ -1,1 +1,10 @@
-console.log(1)
+//如果是第三方模块的话,不用加"./",如果是文件模块,自己写的模块,需要加"./"
+//import 具有声明功能
+//在这个文件中将内容解构出来
+
+//两种方式可以赋值,一种以 对象的解构的方式  一种以* as names
+import {str1, str2} from "./a.js" ;
+console.log(str1,str2)
+
+// import * as vs from "./a.js" ;
+console.log(vs.str1)
